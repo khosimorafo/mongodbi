0b4c0a3b815446a278330a690fc0cd5fe7d94b73
Khosi Morafo <khosimorafo@yahoo.com>
2019-11-09 14:33:24 +0200

    First commit

--- mongodbi_test.go
+++ mongodbi_test.go
@@ -23,38 +23,3 @@ func TestInsert(t *testing.T) {
 
 	assert.Equal(t, mockThing, actual, "should return a Thing")
 }
-
-/*
-var a dbi.App
-
-var url = "mongodb://mastende:mastende@ds115573.mlab.com:15573/mastende-test"
-var dbname = "mastende-test"
-
-// TestMain wraps all tests with the needed initialized mock DB and fixtures
-func TestMain(m *testing.M) {
-
-	// Init test session/db/collection
-	//configor.Load(&mongodbi.Config, "config.yml")
-	a = *dbi.DB(url, dbname)
-	a.Database.C("testcollection").DropCollection()
-
-	// Run the test suite
-	retCode := m.Run()
-
-	// Make sure we DropDatabase so we make absolutely sure nothing is left or locked while wiping the data and
-	// close session
-	a.Session.Close()
-
-	// call with result of m.Run()
-	os.Exit(retCode)
-}
-
-func TestCollectionCreate(t *testing.T) {
-
-	a.SetCollection("testcollection")
-
-	c := a.Collection.Name
-
-	assert.Equal(t, c, "testcollection", "the should be equal")
-}
-*/
